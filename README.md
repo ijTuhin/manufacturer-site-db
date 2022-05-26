9bd09b862dbb60cf06936550da45fb3f9656bc76
ijTuhin <isratishu0209@gmail.com>
2022-05-26 23:21:25 +0600

    added README.md file

---
+++ README.md
@@ -0,0 +1,37 @@
+# Buy-US Manufacturer Site
+    A product manufacturer system. It's a full-stack project,
+    both client/ui & DB/backend developed. Authentication implemented
+    for access control. Different access control for admin & user.
+
+
+## website link
+- https://product-manufacturer-site.web.app/
+
+
+### Frontend/Client site
+- HTML
+- CSS
+- React.js
+- React-router
+- React-router-hooks
+
+### UI design
+- Tailwind css
+- Tailwind css element(component)
+- react-fontAwesome
+- toastify
+- pretty-checkbox-react
+- react-animate-on-scroll
+- animate.css
+- google fonts
+
+### Authentication & hosting
+- Firebase auth (email-password)
+- Firebase auth (google)
+- Firebase hosting
+
+### Backend/Server-DB
+- MongoDB atlas
+- node.js
+- express.js
+- Heroku (deploy server site)
--- index.js
+++ index.js
@@ -9,17 +9,79 @@ console.log(process.env);
 app.use(cors());
 app.use(express.json());
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12dph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('wareHouse').collection('item');
+        app.get('/item', async(req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        /* app.get('/item/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        });
+
+        // POST
+        app.post('/item', async(req, res) =>{
+            const newItem = req.body;
+            const result = await itemCollection.insertOne(newItem);
+            res.send(result);
+        });
 
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('DB connected')
-    client.close();
-  });
+        // update user
+        app.put('/item/:id', async(req, res) =>{
+            const id = req.params.id;
+            const updatedItem = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedItem.quantity,
+                    sold: updatedItem.sold,
+                }
+            };
+            const result = await itemCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
 
+        app.put('/item/:id', async(req, res) =>{
+            const id = req.params.id;
+            const restockItem = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: restockItem.quantity,
+                    
+                }
+            };
+            const result = await itemCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        // DELETE
+        app.delete('/item/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        }); */
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
 
 
 
