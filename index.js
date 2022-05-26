bfe7b61af8da1afd6ddca01c11b718460d52b4b4
ijTuhin <isratishu0209@gmail.com>
2022-05-26 21:47:06 +0600

    DB connected

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+console.log(process.env);
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12dph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('DB connected')
+    client.close();
+  });
+
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Manufacturing Products Server');
+    res.send('Server side!')
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server Running on port ${port}`)
 })
