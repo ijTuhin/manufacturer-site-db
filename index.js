bac02ac09b6afc250aee2528108bf733c24efd8e
ijTuhin <isratishu0209@gmail.com>
2022-05-28 01:12:28 +0600

    getting data from Db

--- index.js
+++ index.js
@@ -15,67 +15,16 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const itemCollection = client.db('wareHouse').collection('item');
-        app.get('/item', async(req, res) => {
+
+        // =============== Reviews =================
+        const reviewCollection = client.db('manufactureDb').collection('review');
+        app.get('/review', async(req, res) => {
             const query = {};
-            const cursor = itemCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         });
 
-        /* app.get('/item/:id', async(req, res) =>{
-            const id = req.params.id;
-            const query={_id: ObjectId(id)};
-            const item = await itemCollection.findOne(query);
-            res.send(item);
-        });
-
-        // POST
-        app.post('/item', async(req, res) =>{
-            const newItem = req.body;
-            const result = await itemCollection.insertOne(newItem);
-            res.send(result);
-        });
-
-
-        // update user
-        app.put('/item/:id', async(req, res) =>{
-            const id = req.params.id;
-            const updatedItem = req.body;
-            const filter = {_id: ObjectId(id)};
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    quantity: updatedItem.quantity,
-                    sold: updatedItem.sold,
-                }
-            };
-            const result = await itemCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        })
-
-        app.put('/item/:id', async(req, res) =>{
-            const id = req.params.id;
-            const restockItem = req.body;
-            const filter = {_id: ObjectId(id)};
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    quantity: restockItem.quantity,
-                    
-                }
-            };
-            const result = await itemCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        })
-
-        // DELETE
-        app.delete('/item/:id', async(req, res) =>{
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const result = await itemCollection.deleteOne(query);
-            res.send(result);
-        }); */
     }
     finally {
 
