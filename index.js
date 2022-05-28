ce820536f41a8960ce735cb90e71fc8e51c22be8
ijTuhin <isratishu0209@gmail.com>
2022-05-28 23:48:38 +0600

    individual data for purchase

--- index.js
+++ index.js
@@ -58,6 +58,12 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         });
+        app.get('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
         app.post('/product', async(req, res) =>{
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
